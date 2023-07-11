64ae1b89d310728b7722d39aff79f70e8c1df5a7
Anthony Bau <dab1998@gmail.com>
2023-07-11 11:40:27 -0400

    Fix typo

--- src/useConnectCall.ts
+++ src/useConnectCall.ts
@@ -426,7 +426,7 @@ const useConnectCall = ({
       if (client) {
         await client.resumeProducer(label);
       } else {
-        const producer = localProducer[label];
+        const producer = localProducers[label];
 
         if (!producer) throw new Error("No such producer");
 
